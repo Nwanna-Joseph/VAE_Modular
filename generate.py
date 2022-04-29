ae547354a47ac81d4bbbe80327894e2cfd3490fe
Nwanna.Joseph <jnwanna@aimsammi.org>
2022-04-29 11:06:59 +0000

    UI fix

--- generate.py
+++ generate.py
@@ -5,4 +5,8 @@ from utils import generate
 if __name__ == '__main__':
     mu_learnt, std_learnt, model = torch.load("./saved_model/mu_tensor.pt")
     text = generate(model, mu_learnt, std_learnt)
-    print(text)
+    print('\n')
+    print("="*100)
+    print("Generated SMILE : ",text)
+    print("=" * 100)
+    print('\n')
