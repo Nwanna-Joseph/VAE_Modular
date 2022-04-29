1528a5158d4a2a7a5138b0ee2c96f2e50816c969
Nwanna.Joseph <jnwanna@aimsammi.org>
2022-04-29 09:29:46 +0000

    bug fixes

--- generate.py
+++ generate.py
@@ -1,3 +1,8 @@
+import torch
+
+from utils import generate
 
 if __name__ == '__main__':
-    pass
+    mu_learnt, std_learnt, model = torch.load("./saved_model/mu_tensor.pt")
+    text = generate(model, mu_learnt, std_learnt)
+    print(text)
--- train.py
+++ train.py
@@ -19,25 +19,34 @@ if __name__ == '__main__':
     loss_array = []
     bce_loss_array = []
     kld_loss_array = []
-    for epoch in range(2):
-        for idx, image in enumerate(trainloader):
-            # images = [print(sequence_to_idx( a ).shape) for a in image ]
-            images = flatten(
-                torch.tensor(np.array([sequence_to_idx(a) for a in image]), dtype=torch.float32).reshape(-1, 1, 1, 35))
+    try:
+        for epoch in range(1):
+            for idx, image in enumerate(trainloader):
+                # images = [print(sequence_to_idx( a ).shape) for a in image ]
+                images = flatten(
+                    torch.tensor(np.array([sequence_to_idx(a) for a in image]), dtype=torch.float32).reshape(-1, 1, 1, 35))
 
-            recon_images, mu, logvar = model(images)
-            mu_learnt = mu
-            std = logvar.mul(0.5).exp_()
-            std_learnt = std
+                recon_images, mu, logvar = model(images)
+                mu_learnt = mu
+                std = logvar.mul(0.5).exp_()
+                std_learnt = std
 
-            loss, bce_loss, kld_loss = loss_fn(recon_images, images, mu, logvar)
-            loss_array.append(loss)
-            bce_loss_array.append(bce_loss)
-            kld_loss_array.append(kld_loss)
+                loss, bce_loss, kld_loss = loss_fn(recon_images, images, mu, logvar)
+                loss_array.append(loss)
+                bce_loss_array.append(bce_loss)
+                kld_loss_array.append(kld_loss)
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
-            if idx % 100 == 0:
-                print("Epoch[{}/{}] Loss: {:.3f}".format(epoch + 1, 2, loss.item() / 32))
+                if idx % 100 == 0:
+                    print("Epoch[{}/{}] Loss: {:.3f}".format(epoch + 1, 2, loss.item() / 32))
+                    # save = {"mu":mu_learnt,"std":std_learnt,"":model}
+                    save = mu_learnt, std_learnt, model
+                    torch.save(save, "./saved_model/mu_tensor.pt")
+
+                    # recon_x, _, _ = model(test)
+                    # save_image(recon_x.view(1, 1, 200, 35).data.cpu(), f'/content/reconstructed/recon_image_{epoch}_{idx}.png')
+    except KeyboardInterrupt:
+        print("Project terminated")
